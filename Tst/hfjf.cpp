2e49f9d83bb86a9e4cd09bc676e0b5812be069d9
ioustreamh <ioustreamh@gmail.com>
2019-09-06 22:11:49 +0430

    Tester and Checker

---
+++ Tst/hfjf.cpp
@@ -0,0 +1,20 @@
+
+  //In The Name Of GOD
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll ;
+const ll mod = 998244353;
+const ll inf = 2000000000;
+const ll infll = (ll)2*1000*1000*1000*1000*1000*1000;
+const ll maxn = 100*1000+1;
+#define pb push_back
+#define pp pop_back
+#define X first
+#define Y second
+#define IO ios_base::sync_with_stdio(false);
+
+int main(){
+  IO;
+  return 259;
+}
