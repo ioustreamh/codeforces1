9155d194cb16a2c77adc3e4ea95639250635aa3d
ioustreamh <ioustreamh@gmail.com>
2019-09-06 21:52:59 +0430

    1

--- code.cpp
+++ code.cpp
@@ -1,5 +1,15 @@
-\*
-nklnmlk
+/*
+what I did
+mad@MLI:~/cpp/CF1$ git clone https://github.com/ioustreamh/codeforces1
+mad@MLI:~/cpp/CF1$ ls
+codeforces1
+mad@MLI:~/cpp/CF1$ cd codeforces1
+mad@MLI:~/cpp/CF1/codeforces1$ ls
+A  code1.cpp  code-1s.cpp  D
+mad@MLI:~/cpp/CF1/codeforces1$ g++ code1.cpp
+mad@MLI:~/cpp/CF1/codeforces1$ atom code1.cpp
+mad@MLI:~/cpp/CF1/codeforces1$ git config --global user.email "ioustreamh@gmail.com"
+mad@MLI:~/cpp/CF1/codeforces1$  git config --global user.name "ioustreamh"
+mad@MLI:~/cpp/CF1/codeforces1$ atom code.cpp
+
 */
-okomo
-*\
